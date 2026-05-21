f5a0fcc2daeb4c7c81d1cc8f4e93052347de0727
welnecker <janiofdr@gmail.com>
2026-05-21 13:09:13 -0300

    Update sheets.py

--- utils/sheets.py
+++ utils/sheets.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import gspread
 
+from google.oauth2.service_account import Credentials
+
+def conectar_planilha():
+
+    scope = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
+
+    creds = Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=scope
+    )
+
+    client = gspread.authorize(creds)
+
+    return client.open_by_key(
+        "19sE4GRsHDovx_daoD1LC2uzQ5VKoRsoaSL3sncQDH6I"
+    )
