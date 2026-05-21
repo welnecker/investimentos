f1c099f239bc13c97a8fbe9c17cc9544039ee23e
welnecker <janiofdr@gmail.com>
2026-05-21 13:11:37 -0300

    Update sheets.py

--- utils/sheets.py
+++ utils/sheets.py
@@ -1,7 +1,14 @@
+# utils/sheets.py
+
 import streamlit as st
 import gspread
+import pandas as pd
 
 from google.oauth2.service_account import Credentials
+
+# =========================================================
+# CONECTAR GOOGLE SHEETS
+# =========================================================
 
 def conectar_planilha():
 
@@ -20,3 +27,19 @@ def conectar_planilha():
     return client.open_by_key(
         "19sE4GRsHDovx_daoD1LC2uzQ5VKoRsoaSL3sncQDH6I"
     )
+
+# =========================================================
+# CARREGAR FIIS
+# =========================================================
+
+def carregar_fiis():
+
+    planilha = conectar_planilha()
+
+    aba = planilha.worksheet("FIIS")
+
+    dados = aba.get_all_records()
+
+    df = pd.DataFrame(dados)
+
+    return df
