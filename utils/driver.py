5d7d68468af022736ccf13289322b00ee27aa867
welnecker <janiofdr@gmail.com>
2026-05-21 13:53:26 -0300

    Implement Chrome debugger connection function
    Added a function to connect to a Chrome debugger.

--- utils/driver.py
+++ utils/driver.py
@@ -1,1 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+# =========================================================
+# CONECTAR CHROME DEBUG
+# =========================================================
+
+def conectar_driver():
+
+    options = Options()
+
+    options.debugger_address = "127.0.0.1:9222"
+
+    driver = webdriver.Chrome(
+        options=options
+    )
+
+    return driver
