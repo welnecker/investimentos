41d68cd6835bb8aef82a91f23176264e5ea9a62a
welnecker <janiofdr@gmail.com>
2026-05-21 13:43:28 -0300

    Update scraper.py

--- utils/scraper.py
+++ utils/scraper.py
@@ -1,1 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
 
+# =========================================================
+# COLETAR FII
+# =========================================================
+
+def coletar_fii(ticker):
+
+    url = f"https://investidor10.com.br/fiis/{ticker.lower()}/"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    response = requests.get(
+        url,
+        headers=headers
+    )
+
+    soup = BeautifulSoup(
+        response.text,
+        "html.parser"
+    )
+
+    dados = {}
+
+    try:
+
+        indicadores = soup.find_all(
+            "div",
+            class_="_card-body"
+        )
+
+        texto = soup.get_text()
+
+        dados["Fundo"] = ticker
+
+        return dados
+
+    except Exception as erro:
+
+        print(f"Erro em {ticker}: {erro}")
+
+        return None
